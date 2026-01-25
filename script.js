d0ac60ab558b10ea5cde5beb7acc7bd4a7d7a3e0
peradzed <datopera77@gmail.com>
2026-01-25 17:51:38 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const form = document.getElementById("inputs_form");
 const loginInput = document.getElementById("loginInput");
 const passwordInput = document.getElementById("passwordInput");
 
-const CORRECT_EMAIL = "test@gmail.com";
-const CORRECT_PASSWORD = "123456";
+const correctEmail = "test@gmail.com";
+const correctPassword = "123456";
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,7 +11,7 @@ form.addEventListener("submit", (e) => {
   const email = loginInput.value.trim();
   const password = passwordInput.value.trim();
 
-  if (email === CORRECT_EMAIL && password === CORRECT_PASSWORD) {
+  if (email === correctEmail && password === correctPassword) {
     localStorage.setItem("isLoggedIn", "true");
     localStorage.setItem("userEmail", email);
 
